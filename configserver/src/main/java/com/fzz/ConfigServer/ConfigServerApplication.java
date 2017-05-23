abcb68132579e4880ab4f6bf7302047f2bf166e9
fangzongzhou <fangzongzhou@thunisoft.com>
2017-05-23 21:07:51 +0800

    瞎搞

---
+++ configserver/src/main/java/com/fzz/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.fzz.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by huayu on 2017/5/22.
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
